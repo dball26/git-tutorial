3a163e5a81bff5b3bb8e7d6577ce234f6bf3f2f9
Drew Ball <dball@issaonline.com>
2022-10-21 14:12:21 -0700

    Make Visible

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const launchTime = new Date('02/28/2023 11:59 PM').getTime()
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const currentTime = new Date().getTime()
+  const numberOfMilliseconds = parseInt(launchTime - currentTime)
+  res.render('index', { title: 'How Long until FAI Site is due?', countDown: numberOfMilliseconds });
 });
 
 module.exports = router;
